d9a13745c2afb898ae96d624fa65bcc942e8b3e9
Ace <ayedawut@gmail.com>
2015-04-16 16:23:35 -0400

    updated the ui

--- Project3/MainWindow.cpp
+++ Project3/MainWindow.cpp
@@ -1,2 +1,13 @@
-#include "MainWindow.h"
+#include "Mainwindow.h" 
 
+using namespace System; 
+using namespace System::Windows::Forms; 
+
+[STAThread] 
+void Main(array<String^>^ args) 
+{ 
+	Application::EnableVisualStyles(); 
+	Application::SetCompatibleTextRenderingDefault(false); 
+	Project3::MainWindow form; 
+	Application::Run(%form);
+}
--- Project3/MainWindow.h
+++ Project3/MainWindow.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
 
 namespace Project3 {
 
@@ -34,6 +37,14 @@ namespace Project3 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TabControl^  tabControl1;
+	protected:
+
+	private: System::Windows::Forms::TabPage^  tabPage2;
+	private: System::Windows::Forms::TabPage^  tabPage1;
+	private: System::Windows::Forms::TabPage^  tabPage3;
+
+	protected:
 
 	private:
 		/// <summary>
@@ -48,18 +59,72 @@ namespace Project3 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage3 = (gcnew System::Windows::Forms::TabPage());
+			this->tabControl1->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this->tabControl1->Controls->Add(this->tabPage2);
+			this->tabControl1->Controls->Add(this->tabPage1);
+			this->tabControl1->Controls->Add(this->tabPage3);
+			this->tabControl1->Location = System::Drawing::Point(12, 12);
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->SelectedIndex = 0;
+			this->tabControl1->Size = System::Drawing::Size(1005, 620);
+			this->tabControl1->TabIndex = 0;
+			// 
+			// tabPage2
+			// 
+			this->tabPage2->Location = System::Drawing::Point(4, 22);
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage2->Size = System::Drawing::Size(997, 594);
+			this->tabPage2->TabIndex = 1;
+			this->tabPage2->Text = L"Geoff";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// tabPage1
+			// 
+			this->tabPage1->Location = System::Drawing::Point(4, 22);
+			this->tabPage1->Name = L"tabPage1";
+			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage1->Size = System::Drawing::Size(997, 594);
+			this->tabPage1->TabIndex = 2;
+			this->tabPage1->Text = L"Ace";
+			this->tabPage1->UseVisualStyleBackColor = true;
+			// 
+			// tabPage3
+			// 
+			this->tabPage3->Location = System::Drawing::Point(4, 22);
+			this->tabPage3->Name = L"tabPage3";
+			this->tabPage3->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage3->Size = System::Drawing::Size(997, 594);
+			this->tabPage3->TabIndex = 3;
+			this->tabPage3->Text = L"James";
+			this->tabPage3->UseVisualStyleBackColor = true;
 			// 
 			// MainWindow
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(477, 322);
+			this->ClientSize = System::Drawing::Size(1270, 644);
+			this->Controls->Add(this->tabControl1);
 			this->Name = L"MainWindow";
 			this->Text = L"MainWindow";
+			this->Load += gcnew System::EventHandler(this, &MainWindow::MainWindow_Load);
+			this->tabControl1->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		MessageBox::Show("something new cool lol losasdls");
+	}
+	private: System::Void tabPage1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
 	};
 }
